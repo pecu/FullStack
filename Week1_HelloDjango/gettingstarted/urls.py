27b298c5bad3f63f98377084822248833818aa50
pecu <pecu610@gmail.com>
2018-09-09 18:09:29 +0800

    update myapp

--- Week1_HelloDjango/gettingstarted/urls.py
+++ Week1_HelloDjango/gettingstarted/urls.py
@@ -4,14 +4,14 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import myapp.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', myapp.views.index, name='index'),
+    url(r'^db', myapp.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
