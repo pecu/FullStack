268effb0cc62ad2e86a3fa9afd15ed27dc7bceee
pecu <pecu610@gmail.com>
2018-09-10 23:42:28 +0800

    update setting db

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -74,13 +74,9 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'CONN_MAX_AGE': 60,  # seconds for persistent connection, since Django 1.6
-    }
+    'default': dj_database_url.config(),
 }
 
 
