a138cb6d6bb8147b2cbe29b846f273adc34fa269
pecu <pecu610@gmail.com>
2018-09-10 23:32:50 +0800

    update setting

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import django_heroku
 
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
